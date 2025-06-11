1eee73ed460d8954a0918c257937cb6bbc01eeb9
Joel Adrianus Lase <xiitkj76@gmail.com>
2025-06-11 14:34:10 +0700

    Update App.test.js
    perbaikan test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test.skip('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test.skip('renders learn react link', () => {
+test('renders Task Manager heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Task Manager/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders empty task message', () => {
+  render(<App />);
+  const message = screen.getByText(/No tasks available/i);
+  expect(message).toBeInTheDocument();
 });
